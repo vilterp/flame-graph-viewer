b5f903a6b9ce22c5f6b33432a6507ede92bc47d4
Pete Vilter <pete.vilter@gmail.com>
2024-10-09 21:50:54 -0400

    flame graph viewer

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import FlameGraph from "react-flame-graph";
+
+function App() {
+  const [json, setJson] = useState("");
+
+  const parsed = tryParse(json);
+
+  return (
+    <div>
+      <h1>Flame Graph Viewer</h1>
+
+      <textarea
+        value={json}
+        style={{ fontFamily: "monospace" }}
+        onChange={(e) => setJson(e.target.value)}
+        placeholder="Paste JSON here"
+      />
+
+      {parsed.type === "err" ? (
+        <p style={{ color: "red" }}>Invalid JSON</p>
+      ) : (
+        <FlameGraph data={json} height={800} width={1000} />
+      )}
+    </div>
+  );
+}
+
+function tryParse(json: string) {
+  try {
+    return { type: "ok", body: JSON.parse(json) };
+  } catch (e) {
+    return { type: "err" };
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
